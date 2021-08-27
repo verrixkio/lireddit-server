df90b36c22367cb5c7e87bfab7304e08c957ce93
Thomas <verrixkio@gmail.com>
2021-08-27 10:11:30 -0600

    add migrator on spiup

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-import { MikroORM, PrimaryKey } from "@mikro-orm/core";
+import { MikroORM} from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
-
+3754
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
-
+  await orm.getMigrator().up();
   const post = orm.em.create(Post, {title: 'my first post'})
   await orm.em.persistAndFlush(post);
-  await orm.em.nativeInsert(Post, {title: 'my second post'})
 };
 
 
