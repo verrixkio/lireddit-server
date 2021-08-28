68332548628feffe93963fb4188717a1a4dec10b
Thomas <verrixkio@gmail.com>
2021-08-28 10:31:54 -0600

    CRUD example using graphql for a post object

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
+import "reflect-metadata";
 import { MikroORM} from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
 import express from 'express';
 import {ApolloServer} from 'apollo-server-express';
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
-import { Server } from "http";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
@@ -16,9 +16,10 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema:  await buildSchema({
-      resolvers: [HelloResolver],
-      validate: false
-    })
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em })
   })
 
   await apolloServer.start()
