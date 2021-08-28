03cbe2b501a472405bb3c798a53c90614feacbc7
Thomas <verrixkio@gmail.com>
2021-08-28 10:32:31 -0600

    file cleanup

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -28,28 +28,27 @@ export class PostResolver {
       return post;
   }
 
-    @Mutation(() => Post)
-    async updatePost(
-      @Arg('id') id: number,
-      @Arg('title', () => String, { nullable: true }) title: string,
-      @Ctx() { em }: MyContext): Promise<Post | null> {
-        const post = await em.findOne(Post, { id });
-        if (!post) {
-          return null
-        }
-        if (typeof title !== 'undefined') {
-          post.title = title;
-          await em.persistAndFlush(post);
-        }
-        return post;
-    }
+  @Mutation(() => Post)
+  async updatePost(
+    @Arg('id') id: number,
+    @Arg('title', () => String, { nullable: true }) title: string,
+    @Ctx() { em }: MyContext): Promise<Post | null> {
+      const post = await em.findOne(Post, { id });
+      if (!post) {
+        return null
+      }
+      if (typeof title !== 'undefined') {
+        post.title = title;
+        await em.persistAndFlush(post);
+      }
+      return post;
+  }
 
-        // Create Post
-        @Mutation(() => Boolean)
-        async deletePost(
-          @Arg('id') id: number,
-          @Ctx() { em }: MyContext): Promise<boolean> {
-            em.nativeDelete(Post, { id });
-            return true;
-        }
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg('id') id: number,
+    @Ctx() { em }: MyContext): Promise<boolean> {
+      em.nativeDelete(Post, { id });
+      return true;
+  }
 }
